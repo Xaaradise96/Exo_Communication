abe8a0c82962462ff5973d29d4acad1e3c061877
xaaradise96 <alisonshanna.ah@gmail.com>
2019-11-19 16:09:21 +0100

    prog led

---
+++ local/affiche.py
@@ -0,0 +1,11 @@
+import ledArduino
+import dicoMatrice2D
+
+print("Quelle lettre afficher?")
+lettre = input(">>>")
+matrice = dicoMatrice2D.dico2D[lettre]
+
+for ligne in matrice:
+    print(ligne)
+
+ledArduino.ledMatrice(matrice)
--- local/ledArduino.py
+++ local/ledArduino.py
@@ -1,13 +1,6 @@
 import i2cComm
 
-lettre = "a"
-matrice = [
-            [1,1,1,1,1],
-            [1,0,0,0,1],
-            [1,1,1,1,1],
-            [1,0,0,0,1],
-            [1,0,0,0,1] 
-            ]
-for ligne in matrice:
-    for led in ligne:
-        i2cComm.writeNumber(led)
+def ledMatrice (matrice):
+    for ligne in matrice:
+        for led in ligne:
+            i2cComm.writeNumber(led)
